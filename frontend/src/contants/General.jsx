8d6978d2c6ba2e271102ce7afc9d15e741bf6a4b
Nghia Nguyen <NghiaNguyen@NghiaNhs-MacBook-Pro-2.local>
2022-03-27 12:51:38 +0700

    processing axios

---
+++ frontend/src/contants/General.jsx
@@ -0,0 +1,13 @@
+//---------import constant---------
+
+import * as constantGeneral from "../constants/General"
+
+class ActionGeneral {
+    /*setIsProcessing(isProcessing) {
+        return function (dispatch) {
+            dispatch({type: constantGeneral.GENERAL_SET_STATUS_PROCESSING, isProcessing: isProcessing});
+        }
+    }*/
+}
+
+export default new ActionGeneral();
